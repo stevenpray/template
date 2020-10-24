10f3b48043c399245d797c4772d94a942da2447d
Steven Pray <steven@stevenpray.com>
2020-10-24 17:56:02 -0400

    refactor: remove unnecessary file index.js

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 
 require("dotenv-expand")(require("dotenv").config());
-const { main } = require("..");
+const { pathExistsSync } = require("fs-extra");
+const root = require("app-root-path");
 
-main(process.argv).catch((error) => {
+/**
+ * @type {import("../src")}
+ */
+const lib = (() => {
+  if (pathExistsSync(root.resolve("lib/index.js"))) {
+    return root.require("lib");
+  }
+  require("@babel/register")({ extensions: [".js", ".ts"] });
+  return root.require("src");
+})();
+
+lib.main(process.argv).catch((error) => {
   console.error(error);
   process.exit(error?.code ?? 1);
 });
