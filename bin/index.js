131518cef5ba54068fc7631f00dc4fbf6143acb6
Steven Pray <steven@stevenpray.com>
2020-12-24 13:37:51 -0500

    refactor: create single package entrypoint

--- bin/index.js
+++ bin/index.js
@@ -2,20 +2,4 @@
 
 "use strict";
 
-const fs = require("fs-extra");
-const root = require("app-root-path");
-require("../env");
-
-/**
- * @type {import("../src")}
- */
-const lib = (() => {
-  if (fs.pathExistsSync(root.resolve("lib"))) {
-    return root.require("lib");
-  }
-  const register = require("@babel/register");
-  register({ extensions: [".js", ".ts"] });
-  return root.require("src");
-})();
-
-void lib.cli(process.argv);
+void require("..").cli(process.argv);
---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const fs = require("fs-extra");
+const root = require("app-root-path");
+require("./env");
+
+/**
+ * @type {import("./src")}
+ */
+const lib = (() => {
+  if (fs.pathExistsSync(root.resolve("lib"))) {
+    return root.require("lib");
+  }
+  const register = require("@babel/register");
+  register({ extensions: [".js", ".ts"] });
+  return root.require("src");
+})();
+
+module.exports = lib;
