a5a649472e88d6e6ebcc0204157b14dbeec0eb33
Steven Pray <steven@stevenpray.com>
2021-01-02 17:28:42 -0500

    feat: add context class

---
+++ src/context.ts
@@ -0,0 +1,23 @@
+import root from "app-root-path";
+import type { Paths } from "env-paths";
+import paths from "env-paths";
+import type { PackageJson, SetRequired } from "type-fest";
+
+type Dir = Paths & { run?: string };
+type Env = NonNullable<NodeJS.ProcessEnv["NODE_ENV"]>;
+type Pkg = SetRequired<PackageJson, "name" | "version">;
+
+const pkg = root.require("package.json") as Pkg;
+
+export class Context {
+  readonly dir: Dir;
+  readonly env: Env;
+  readonly pkg: Pkg;
+
+  constructor() {
+    // eslint-disable-next-line node/no-process-env
+    this.env = process.env.NODE_ENV ?? "development";
+    this.pkg = pkg;
+    this.dir = paths(this.pkg.name, { suffix: "" });
+  }
+}
