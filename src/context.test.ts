8a95e50b5ce8d759f9216f709f19bbdfb7b818dd
Steven Pray <steven@stevenpray.com>
2021-01-02 22:22:41 -0500

    test: add context.test

---
+++ src/context.test.ts
@@ -0,0 +1,24 @@
+import { Context } from "./context";
+
+describe("Context", () => {
+  it("should be exported class", () => {
+    expect.assertions(2);
+    expect(Context).toBeFunction();
+    expect(new Context()).toBeInstanceOf(Context);
+  });
+
+  it("should define dir property", () => {
+    expect.assertions(1);
+    expect(new Context().dir).toBeObject();
+  });
+
+  it("should define env property", () => {
+    expect.assertions(1);
+    expect(new Context().env).toStrictEqual("test");
+  });
+
+  it("should define pkg property", () => {
+    expect.assertions(1);
+    expect(new Context().pkg).toBeObject();
+  });
+});
