76819dbacb41fc12d821e20f8e45d8a30eb614ea
Steven Pray <steven@stevenpray.com>
2022-09-03 21:07:06 -0400

    fix(bin): add env split string option

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S node --title=template --enable-source-maps --no-warnings --unhandled-rejections=throw
 
-// eslint-disable-next-line node/shebang,lines-around-directive
+// eslint-disable-next-line lines-around-directive, node/shebang -- Linux requires the `-S` option on env to split argument string.
 "use strict";
 
-void require("..").cli();
+void require("..").cli.exec();
